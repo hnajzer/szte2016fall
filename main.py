5ec93249c86ee88c00d71a36d4a577b77d4d0ebf
knif3 <knif3@envio.hu>
2016-10-05 08:59:39 +0200

    pyhton sample

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-#from flask import Flask
-import os
-
-#from blueprints.movies import movies
-#from model.movies import Movies
-
-#app = Flask(__name__)
-
-#app.movies = Movies()
-
-
-#@app.route('/')
-def hello_proba():
-    print 'Ha ezt latod, akkor minden jo!'
+def greet(name):
+    print 'Hello, %s!' % name
     return;
 
-hello_proba()
-
-#app.register_blueprint(movies, url_prefix='/movies')
-
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=os.getenv('PORT', None))
+name = raw_input('Hogy hivnak?\n')
+greet(name)
