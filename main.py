00238b686a52db25282936bfaa128273e41c6b3d
knif3 <knif3@envio.hu>
2016-10-05 14:53:47 +0200

    sokadik teszt

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-#import os
+import os
+from flask import Flask
 
-print "Content-type: text/html"
+app = Flask(__name__)
 
-def main():
-  print "Nagyon egyszeru script, ami talan le is fut..."
-
+@app.route('/')
+def hello():
+    return 'Hali!'
 
 if __name__ == '__main__':
-  main()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
