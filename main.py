ef6ee2c86b793fd09f72e8d0cd915a6597270d2d
knif3 <knif3@envio.hu>
2016-10-05 08:50:12 +0200

    teszt-kommit

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ import os
 from blueprints.movies import movies
 from model.movies import Movies
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
-app.movies = Movies()
+#app.movies = Movies()
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_proba():
+    print 'Ha ezt látod, akkor minden jó!'
+    return;
 
+hello_proba()
 
-app.register_blueprint(movies, url_prefix='/movies')
+#app.register_blueprint(movies, url_prefix='/movies')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv('PORT', None))
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=os.getenv('PORT', None))
