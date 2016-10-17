f8ac93ddea78824bd9d539b903bc132661e5d57e
matepp <matepp95@gmail.com>
2016-10-17 17:29:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-from flask import Flask
-import os
+return 'Hello, World!'
 
-from blueprints.movies import movies
-from model.movies import Movies
-
-app = Flask(__name__)
-
-app.movies = Movies()
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-
-app.register_blueprint(movies, url_prefix='/movies')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.getenv('PORT', None))
