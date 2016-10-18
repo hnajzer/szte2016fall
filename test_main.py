14ec193ee65bde905f6ad94a773bc7aebfc115ea
Szpisják Viktor <vszpisjak@gmail.com>
2016-10-18 21:17:48 +0200

    4. homework test fix.

--- test_main.py
+++ test_main.py
@@ -21,7 +21,7 @@ class MainTest(unittest.TestCase):
 
     def test_hello(self):
         rv = self.app.get('/')
-        assert "Hello, World!" in rv.data
+        assert "Hello continuous delivery" in rv.data
 
     def test_get_movie_nonexisting(self):
         response = self.app.get('/movies/1')
