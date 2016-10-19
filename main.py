4638b71075469dc3b1858569aee2dc53fd91a9e0
matepp <matepp95@gmail.com>
2016-10-19 14:48:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
- from flask import Flask
- import os
+from flask import Flask
+import os
  
   
- from blueprints.movies import movies
- from model.movies import Movies
+from blueprints.movies import movies
+from model.movies import Movies
  
- app = Flask(__name__)
+app = Flask(__name__)
  
- app.movies = Movies()
+app.movies = Movies()
  
 
- @app.route('/')
- def hello_world():
-     return 'Hello, World!'
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
  
  
- app.register_blueprint(movies, url_prefix='/movies')
+app.register_blueprint(movies, url_prefix='/movies')
  
- if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=os.getenv('PORT', None))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT', None))
 
