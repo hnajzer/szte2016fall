97de180b583c2621f3e534e2915acc236f0c59a7
andrasmarkus <andrasmarkus94@gmail.com>
2016-10-20 17:19:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.movies = Movies()
 
 @app.route('/')
 def hello_world():
-    return 'Hello continuous delivery!'
+    return 'Hello, continuous delivery!'
 
 
 app.register_blueprint(movies, url_prefix='/movies')
