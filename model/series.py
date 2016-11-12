ce3473a6517c47daa97767817c935e607922c4a6
Nagy Richárd @Richardson35 <riiichj@gmail.com>
2016-11-12 22:36:36 +0100

    8. hazi - bug fix4

--- model/series.py
+++ model/series.py
@@ -1,37 +1,38 @@
-class Series():
+class Series:
     def __init__(self):
-        self.series = {}
-        self.id = 0
-
-    def _does_series_exist(self, id):
-        return id in self.series
-
-    def _get_next_id(self):
-        self.id = self.id + 1
-        return self.id
-
-    def create_series(self, data):
-        nextId = self._get_next_id()
-        data = data.copy()
-        data['id'] = nextId
-        self.series[nextId] = data
-        return self.series[nextId]
-
+        self.series = []
+    
+    def create_series(self, adat):
+        data['id'] = len(self.series)+1
+        self.series.append(adat)
+        return self.series[-1]
+    
     def get_series(self, id):
         if self._does_series_exist(id):
-            return self.series[id]
+            return self.series[id-1]
         return False
 
-    def update_series(self, id, data):
+    def _does_series_exist(self, id):
+        if not self.series[id-1]:
+            return False
+        else:
+            return True   
+    
+    def update_series(self, id, adat):
         if not self._does_series_exist(id):
             return False
-
-        self.series[id] = data
-        return self.series[id]
-
+        for i, j in adat.items():
+            self.series[id-1][i] = j
+        return self.series[id-1]
+    
     def delete_series(self, id):
         if not self._does_series_exist(id):
             return False
+        self.series[id-1] = None
+        return True
 
-    del self.series[id]
-	return True
+    def get_all_series(self):
+        return self.series
+
+    def delete_all_series(self):
+        self.series = []
