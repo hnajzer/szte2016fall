31795370b4bc12cb2ac49cbc69512c67e3ed69b9
andrasmarkus <andrasmarkus94@gmail.com>
2016-11-16 23:46:00 +0100

    Update movies.py

--- model/movies.py
+++ model/movies.py
@@ -1,22 +1,37 @@
-from pymongo import MongoClient
-
 class Movies():
-    # mongo ds155727.mlab.com:55727/piank1773 -u <dbuser> -p <dbpassword>'
     def __init__(self):
-        client = MongoClient('ds155727.mlab.com', 55727)
-        client['piank1773'].authenticate('admin', 'admin')
-        db = client['piank1773']
-        self.movies = db.movies
-        self.create_movie({"title": "aSD", "year": 1995})
+        self.movies = {}
+        self.id = 0
+
+    def _does_movie_exist(self, id):
+        return id in self.movies
+
+    def _get_next_id(self):
+        self.id = self.id + 1
+        return self.id
 
     def create_movie(self, data):
-        return self.movies.insert_one(data).inserted_id
+        nextId = self._get_next_id()
+        data = data.copy()
+        data['id'] = nextId
+        self.movies[nextId] = data
+        return self.movies[nextId]
 
     def get_movie(self, id):
-        return self.movies.find_one({'_id': id})
+        if self._does_movie_exist(id):
+            return self.movies[id]
+        return False
 
     def update_movie(self, id, data):
-        return self.movies.find_one_and_replace({'_id': id}, data)
+        if not self._does_movie_exist(id):
+            return False
+
+        self.movies[id] = data
+        return self.movies[id]
 
     def delete_movie(self, id):
-        return self.movies.delete_one({'_id': id})
+        if not self._does_movie_exist(id):
+            return False
+
+        del self.movies[id]
+        return True
