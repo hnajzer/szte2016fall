adcfe396160b4a82204992c20f22240f74068fcc
Nagy Richárd @Richardson35 <riiichj@gmail.com>
2016-11-17 22:40:11 +0100

    mongo change movie

--- model/movies.py
+++ model/movies.py
@@ -1,37 +1,22 @@
+from pymongo import MongoClient
+
 class Movies():
+    #m.lab
     def __init__(self):
-        self.movies = {}
-        self.id = 0
-
-    def _does_movie_exist(self, id):
-        return id in self.movies
-
-    def _get_next_id(self):
-        self.id = self.id + 1
-        return self.id
+        client = MongoClient('ds011495.mlab.com', 11495)
+        client['szte2016fall'].authenticate('ricsi', 'ricsi123')
+        db = client['szte2016fall']
+        self.movies = db.movies
 
     def create_movie(self, data):
-        nextId = self._get_next_id()
-        data = data.copy()
-        data['id'] = nextId
-        self.movies[nextId] = data
-        return self.movies[nextId]
+        return self.movies.insert_one(data).inserted_id
 
     def get_movie(self, id):
-        if self._does_movie_exist(id):
-            return self.movies[id]
-        return False
+        return self.movies.find_one({'_id': id})
 
     def update_movie(self, id, data):
-        if not self._does_movie_exist(id):
-            return False
-
-        self.movies[id] = data
-        return self.movies[id]
+        return self.movies.find_one_and_replace({'_id': id}, data)
 
     def delete_movie(self, id):
-        if not self._does_movie_exist(id):
-            return False
+        return self.movies.delete_one({'_id': id})
 
-        del self.movies[id]
-        return True
