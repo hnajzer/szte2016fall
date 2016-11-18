3450bde439dc84ec60fbcb5d290e2efc43946b16
Nagy Richárd @Richardson35 <riiichj@gmail.com>
2016-11-18 23:42:46 +0100

    movies update6

--- blueprints/movies.py
+++ blueprints/movies.py
@@ -18,7 +18,17 @@ def not_found():
     return get_error('Movie not found!', 404)
 
 
-def parse_movie(data, id):
+def parse_movie(data):
+    movie = {}
+    if 'title' in data:
+        movie['title'] = data['title']
+    if 'year' in data:
+        movie['year'] = data['year']
+    if 'director' in data:
+        movie['director'] = data['director']
+    return movie
+
+def parse_movie_with_id(data, id): 
     movie = {}
     if 'title' in data:
         movie['title'] = data['title']
@@ -42,20 +52,20 @@ def get_movie(movie_id):
 
 @movies.route('/', methods=['POST'])
 def post_movie():
-    movie_data = parse_movie(request.get_json(), '')
+    movie_data = parse_movie(request.get_json())
     # ide kell azt str() fuggveny a TypeError ObjectId miatt (Tamas help e-mailben)
     movie = str(current_app.movies.create_movie(movie_data))
     if not movie:
         return existing()
-    completed_create_movie = parse_movie(current_app.movies.get_movie(movie), movie)
+    completed_create_movie = parse_movie_with_id(current_app.movies.get_movie(movie), movie)
     return jsonify(movie)
 
 
 @movies.route('/<int:movie_id>', methods=['PATCH'])
 def patch_movie(movie_id):
-    movie_data = parse_movie(request.get_json(), '')
+    movie_data = parse_movie(request.get_json())
     current_app.movies.update_movie(ObjectId(movie_id), movie_data)
-    movie = parse_movie(current_app.movies.get_movie(movie_id), movie_id)
+    movie = parse_movie_with_id(current_app.movies.get_movie(movie_id), movie_id)
     if not movie:
         return not_found()
     return jsonify(movie)
