77e126c93caf2a001f51ebc1851e2867011889e3
Nagy Richárd @Richardson35 <riiichj@gmail.com>
2016-11-21 21:58:35 +0100

    9. homework - 0.0.2

--- test_main.py
+++ test_main.py
@@ -8,35 +8,37 @@ from bson import json_util
 
 import main
 from model.movies import Movies
+from model.mongo import Movies
+from model.users import Users
 
 class MainTest(unittest.TestCase):
     def setUp(self):
-        self.a_movie_data = {"title": "Interstellar", "year": 2014, "director": "Christopher Nolan"}
+        self.a_user_data = {"username": "ricsi123", "password": "123", "login": 0}
         main.app.config['TESTING'] = True
         self.app = main.app.test_client()
 
     def tearDown(self):
-        self.app.application.movies = Movies()
+        self.app.application.users = Users()
 
     def test_hello(self):
         rv = self.app.get('/')
         assert b"9. homework - users, login" in rv.data    
 
-    def test_create_new_movie(self):
-        response = self.app.post('/movies/'
-                                 , data=json.dumps(self.a_movie_data)
+    def test_register_new_user(self):
+        response = self.app.post('/users/'
+                                 , data=json.dumps(self.a_user_data)
                                  , content_type='application/json')
         assert response.status_code == 200
 
-    def test_create_new_movie_with_mock(self):
-        self.app.application.movies = Mock()
-        self.app.application.movies.create_movie = Mock(return_value=self.a_movie_data)
+    #def test_create_new_movie_with_mock(self):
+    #    self.app.application.movies = Mock()
+    #    self.app.application.movies.create_movie = Mock(return_value=self.a_movie_data)
 
-        self.app.post('/movies/'
-                      , data=json.dumps(self.a_movie_data)
-                      , content_type='application/json')
+    #    self.app.post('/movies/'
+    #                  , data=json.dumps(self.a_movie_data)
+    #                  , content_type='application/json')
 
-        self.app.application.movies.create_movie.assert_called_once_with(self.a_movie_data)
+    #   self.app.application.movies.create_movie.assert_called_once_with(self.a_movie_data)
 
 
 if __name__ == '__main__':
