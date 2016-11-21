78b060a4fdbd8cae933b4aa2887fae3d08a49f8a
Nagy Richárd @Richardson35 <riiichj@gmail.com>
2016-11-21 22:48:39 +0100

    9. homework - 0.1.2

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from blueprints.movies import movies
 from blueprints.series import series
 from blueprints.users import users
 from model.mongo import Movies
+from model.mongo import Users
 from model.series import Series
 
 app = Flask(__name__)
@@ -21,7 +22,7 @@ app.register_blueprint(movies, url_prefix='/movies')
 
 app.register_blueprint(series, url_prefix='/series')
 
-app.register_blueprint(series, url_prefix='/users')
+app.register_blueprint(users, url_prefix='/users')
 
 if __name__ == '__main__':
     app.run()
