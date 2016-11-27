3dde89a4d80ba4542415ddcdff2dd0c229348900
Nagy Richárd @Richardson35 <riiichj@gmail.com>
2016-11-27 17:41:31 +0100

    9. homework - 1.0.5

--- model/mongo.py
+++ model/mongo.py
@@ -4,11 +4,11 @@ from werkzeug.security import generate_password_hash, \
 import copy
 class Movies():
 
-#    def __init__(self):
-#	client = MongoClient('ds011495.mlab.com', 11495)
-#	client['szte2016fall'].authenticate('ricsi', 'ricsi123')
-#	db = client['szte2016fall']
-#	self.movies = db.movies
+    def __init__(self):
+	client = MongoClient('ds011495.mlab.com', 11495)
+	client['szte2016fall'].authenticate('ricsi', 'ricsi123')
+	db = client['szte2016fall']
+	self.movies = db.movies
 
     def create_movie(self, data):
         return self.movies.insert_one(data).inserted_id
@@ -22,33 +22,11 @@ class Movies():
     def delete_movie(self, id):
         return self.movies.delete_one({'_id': id})
 
-<<<<<<< HEAD
 class Users():
-=======
-class Users(object):
 
 	def __init__(self, username, password):
 		self.username = username
 		self.set_password(password)
-		client = MongoClient('ds011495.mlab.com', 11495)
-		client['szte2016fall'].authenticate('ricsi', 'ricsi123')
-		db = client['szte2016fall']		
-		self.users = db.users
-
-	def set_password(self, password):
-		self.pw_hash = generate_password_hash(password)
-
-	def check_password(self, password):
-		return check_password_hash(self.pw_hash, password)
-
-	def registration(self):		
-		new_user_doc = {"name": self.username, "pass": self.pw_hash}
-		return self.users.insert_one(new_user_doc).inserted_id
-
-#Only for testing
->>>>>>> ebe78992850354071a21354319f314ff746c304c
-
-	def __init__(self):
 		client = MongoClient('ds011495.mlab.com', 11495)
 		client['szte2016fall'].authenticate('ricsi', 'ricsi123')
 		db = client['szte2016fall']		
