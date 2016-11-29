16e5d46e3a220a1be507f2af75ef6aa2725b0066
aurell1 <burik.aurel@stud.u-szeged.hu>
2016-11-29 16:09:46 +0100

    Hazi 10 - Health v1.0

--- hello.py
+++ hello.py
@@ -2,14 +2,17 @@ from flask import Flask
 import os
 
 from blueprints.movies import movies
+from blueprints.health import health
 from blueprints.users import users, login_required
 from model.movies import Movies
 from model.users import Users
+from model.health import Health
 
 app = Flask(__name__)
 
 app.movies = Movies()
 app.users = Users()
+app.health = Health()
 
 
 @app.route('/')
@@ -24,6 +27,7 @@ def secret():
 
 app.register_blueprint(movies, url_prefix='/movies')
 app.register_blueprint(users, url_prefix='/users')
+app.register_blueprint(health, url_prefix='/health')
 
 app.config['SESSION_TYPE'] = 'filesystem'
 app.secret_key = 'secretttt'
